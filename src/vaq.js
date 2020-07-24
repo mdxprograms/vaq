511712354f4ec4dfb09e7c35a915a1a20c51ab0e
Josh Waller <mdx.programs@gmail.com>
2020-07-24 15:08:50 -0400

    Adding new libs and lib folder

--- src/vaq.js
+++ src/vaq.js
@@ -1,14 +1,40 @@
 import axios from 'axios'
+import figlet from 'figlet'
+import figStandard from 'figlet/importable-fonts/Standard.js'
 
 // vim awesome api
 const API = 'https://vimawesome.com/api/plugins'
 
-const fetchPlugins = (pageNum = 1) =>
+const fetchPluginData = (pageNum = 1, query = '') =>
   axios
-    .get(`${API}?page=${pageNum}`)
+    .get(`${API}?page=${pageNum}&query=${query}`)
     .then((res) => res.data)
     .catch((err) => console.log(err))
 
-fetchPlugins().then((plugs) => {
-  plugs.forEach((p) => console.log(p))
-})
+const fetchAllPlugins = () =>
+  fetchPluginData().then((data) => {
+    console.log(data.plugins)
+  })
+
+const sendCliHeading = () => {
+  figlet.parseFont('Standard', figStandard)
+
+  figlet.text(
+    'Vaq (vim awesome query)',
+    {
+      font: 'Standard'
+    },
+    (err, data) => {
+      if (err) {
+        console.log('Figlet failed: ', err)
+        return
+      }
+
+      return console.log(data)
+    }
+  )
+}
+
+// fetchAllPlugins()
+
+sendCliHeading()
