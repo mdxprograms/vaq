ae24a3f3f62cbafc5fb5d2d6ab13547f7cf2ab9a
Josh Waller <mdx.programs@gmail.com>
2020-07-26 19:03:08 -0400

    Added uninstall, back, exit, info, and updated list

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
+/* eslint-disable no-case-declarations */
 
 const figlet = require('figlet')
 const clear = require('clear')
 
-const { infoMsg, successMsg } = require('./lib/colorStates')
-const { savePlugin } = require('./lib/files')
-const { fetchAllPlugins, queryPlugins } = require('./lib/api')
+const { infoMsg, errorMsg, successMsg } = require('./lib/colorStates')
+const { savePlugin, removePlugin } = require('./lib/files')
+const { getPlugNameFromConfig } = require('./lib/helpers')
+const { fetchPluginDetails, queryPlugins } = require('./lib/api')
 const {
   askForVimInfo,
   askForQuery,
   askPluginSelection,
+  installedPluginsList,
   menuSelection
 } = require('./lib/prompts')
 const store = require('./lib/store')
@@ -32,10 +35,40 @@ const runCommand = async (command) => {
   }
 
   if (command === 'list') {
-    const results = await fetchAllPlugins()
-    const { selection } = await askPluginSelection(results.plugins)
+    const { plugin, action } = await installedPluginsList()
 
-    savePlugin(selection)
+    switch (action) {
+      case 'update':
+        break
+
+      case 'info':
+        const details = await fetchPluginDetails(getPlugNameFromConfig(plugin))
+        Object.keys(details).forEach((val) =>
+          infoMsg(`${val}: ${details[val]}`)
+        )
+        runMenu()
+        break
+
+      case 'uninstall':
+        removePlugin(plugin)
+        runMenu()
+        break
+
+      case 'back':
+        runMenu()
+        break
+
+      case 'exit':
+        return false
+
+      default:
+        errorMsg('Invalid action for plugin')
+        return false
+    }
+  }
+
+  if (command === 'exit') {
+    return false
   }
 }
 
